04cafc1914f44e4ad05d86a3f30e116fd40a873b
Zack Kuntz <zkuntz@peoplenetonline.com>
2015-12-10 12:50:39 -0600

    day 6

--- day1/day1.js
+++ day1/day1.js
@@ -1,1 +1,4 @@
-console.log(process.argv[2].split('').reduce((r, v, i) => v === '(' ? {r: r.r+1} : {r: r.r-1}, {r: 0}))
+console.log(process.argv[2].split('').reduce((r, v, i) => v === '(' ? {r: r.r + 1} : {r: r.r - 1}, {r: 0}))
+
+console.log(process.argv[2].split('').reduce((r, v) => r + {'(': 1, ')': -1}[v], 0))
+console.log(process.argv[2].split('').reduce((r, v) => v == '(' ? r + 1 : r - 1, 0))
--- day6/day6b.js
+++ day6/day6b.js
@@ -1,3 +1,17 @@
-/**
- * Created by zackkuntz on 12/8/15.
- */
+var fs = require('fs')
+var obj = fs.readFileSync('input', 'utf8').split('\n').reduce((r, v) => {
+    var lights = /(\w*)\s(\d*),(\d*)\s(\w*)\s(\d*),(\d*)/g.exec(v)
+    for(i=parseInt(lights[2]);i<=parseInt(lights[5]);i++) {
+        for (j = parseInt(lights[3]); j <= parseInt(lights[6]); j++) {
+            if(lights[1]=='on'){
+                r[`${i},${j}`] = r[`${i},${j}`] ? r[`${i},${j}`]+1 : 1
+            } else if(lights[1]==='off'){
+                r[`${i},${j}`] = r[`${i},${j}`] > 0 ? r[`${i},${j}`]-1 : 0
+            } else if(lights[1]==='toggle'){
+                r[`${i},${j}`] = r[`${i},${j}`] ? r[`${i},${j}`]+2 : 2
+            }
+        }
+    }
+    return r
+},{})
+console.log(Object.keys(obj).reduce((r,v)=> r+obj[v],0))
