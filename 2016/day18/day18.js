313bc5786bf2d48a54323d8bc3d05c6d131ea4a4
Zack Kuntz <zkuntz@peoplenetonline.com>
2016-12-18 22:52:58 -0600

    2016 day 18

---
+++ 2016/day18/day18.js
@@ -0,0 +1,29 @@
+let input = require('fs').readFileSync('input', 'utf-8')
+let _ = require('lodash')
+
+let cur = input.split('')
+let prev = []
+let safe = _.filter(cur, v => v == '.').length
+
+let trap = (left, center, right) => {
+    if (right != '^' && center === '^' && left == '^') {
+        return '^'
+    } else if (right === '^' && center === '^' && left !== '^') {
+        return '^'
+    } else if (right === '^' && center !== '^' && left !== '^') {
+        return '^'
+    } else if (right !== '^' && center !== '^' && left === '^') {
+        return '^'
+    }
+    return '.'
+
+}
+
+for (let i = 0; i < 39; i++) {
+    prev = _.clone(cur)
+    for (let n = 0; n < cur.length; n++) {
+        cur[n] = trap(prev[n - 1], prev[n], prev[n + 1])
+    }
+    safe += _.filter(cur, v => v == '.').length
+}
+console.log(safe)
